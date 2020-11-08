3d21cee43da632469678e5a97f0cf1b5a4844cd7
HydroGames-dev <69600115+HydroGames-dev@users.noreply.github.com>
2020-11-09 00:51:19 +0530

    Add files via upload

---
+++ src/alvin0319/AmongUs/entity/EntityJoin.php
@@ -0,0 +1,14 @@
+<?php 
+
+namespace alvin0319\AmongUs\entity;
+
+use pocketmine\entity\Human;
+use pocketmine\entity\Entity;
+use alvin0319\AmongUs\AmongUs;
+
+class EntityJoin extends Human {
+	
+	public function getName() : String{
+		return "Name";
+	}
+}
