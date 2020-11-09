47091bec08af17fdad751e35e55cadcad55f8a92
HydroGames-dev <69600115+HydroGames-dev@users.noreply.github.com>
2020-11-10 01:09:07 +0530

    Update 0.0.12

---
+++ src/alvin0319/AmongUs/libs/muqsit/invmenu/session/PlayerNetwork.php
@@ -0,0 +1,150 @@
+<?php
+
+/*
+ *  ___            __  __
+ * |_ _|_ ____   _|  \/  | ___ _ __  _   _
+ *  | || '_ \ \ / / |\/| |/ _ \ '_ \| | | |
+ *  | || | | \ V /| |  | |  __/ | | | |_| |
+ * |___|_| |_|\_/ |_|  |_|\___|_| |_|\__,_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author Muqsit
+ * @link http://github.com/Muqsit
+ *
+*/
+
+declare(strict_types=1);
+
+namespace alvin0319\AmongUs\libs\muqsit\invmenu\session;
+
+use Closure;
+use Ds\Queue;
+use InvalidArgumentException;
+use alvin0319\AmongUs\libs\muqsit\invmenu\session\network\handler\PlayerNetworkHandler;
+use alvin0319\AmongUs\libs\muqsit\invmenu\session\network\NetworkStackLatencyEntry;
+use pocketmine\network\mcpe\protocol\NetworkStackLatencyPacket;
+use pocketmine\Player;
+
+final class PlayerNetwork{
+
+	/** @var Player */
+	private $session;
+
+	/** @var Queue<NetworkStackLatencyEntry> */
+	private $queue;
+
+	/** @var NetworkStackLatencyEntry|null */
+	private $current;
+
+	/** @var PlayerNetworkHandler */
+	private $handler;
+
+	/** @var int */
+	private $graphic_wait_duration = 50 * 5;
+
+	public function __construct(Player $session, PlayerNetworkHandler $handler){
+		$this->session = $session;
+		$this->handler = $handler;
+		$this->queue = new Queue();
+	}
+
+	public function getGraphicWaitDuration() : int{
+		return $this->graphic_wait_duration;
+	}
+
+	/**
+	 * Duration (in milliseconds) to wait between sending the graphic (block)
+	 * and sending the inventory.
+	 *
+	 * @param int $graphic_wait_duration
+	 */
+	public function setGraphicWaitDuration(int $graphic_wait_duration) : void{
+		if($graphic_wait_duration < 0){
+			throw new InvalidArgumentException("graphic_wait_duration must be >= 0, got {$graphic_wait_duration}");
+		}
+
+		$this->graphic_wait_duration = $graphic_wait_duration;
+	}
+
+	public function dropPending() : void{
+		foreach($this->queue as $entry){
+			($entry->then)(false);
+		}
+		$this->queue->clear();
+		$this->setCurrent(null);
+	}
+
+	/**
+	 * @param Closure $then
+	 *
+	 * @phpstan-param Closure(bool) : void $then
+	 */
+	public function wait(Closure $then) : void{
+		$entry = $this->handler->createNetworkStackLatencyEntry($then);
+		if($this->current !== null){
+			$this->queue->push($entry);
+		}else{
+			$this->setCurrent($entry);
+		}
+	}
+
+	/**
+	 * Waits at least $wait_ms before calling $then(true).
+	 *
+	 * @param int $wait_ms
+	 * @param Closure $then
+	 * @param int|null $since_ms
+	 *
+	 * @phpstan-param Closure(bool) : void $then
+	 */
+	public function waitUntil(int $wait_ms, Closure $then, ?int $since_ms = null) : void{
+		if($since_ms === null){
+			$since_ms = (int) floor(microtime(true) * 1000);
+		}
+		$this->wait(function(bool $success) use($since_ms, $wait_ms, $then) : void{
+			if($success && ((microtime(true) * 1000) - $since_ms) < $wait_ms){
+				$this->waitUntil($wait_ms, $then, $since_ms);
+			}else{
+				$then($success);
+			}
+		});
+	}
+
+	private function setCurrent(?NetworkStackLatencyEntry $entry) : void{
+		if($this->current !== null){
+			$this->processCurrent(false);
+			$this->current = null;
+		}
+
+		if($entry !== null){
+			$pk = new NetworkStackLatencyPacket();
+			$pk->timestamp = $entry->network_timestamp;
+			$pk->needResponse = true;
+			if($this->session->sendDataPacket($pk)){
+				$this->current = $entry;
+			}else{
+				($entry->then)(false);
+			}
+		}
+	}
+
+	private function processCurrent(bool $success) : void{
+		if($this->current !== null){
+			($this->current->then)($success);
+			$this->current = null;
+			if(!$this->queue->isEmpty()){
+				$this->setCurrent($this->queue->pop());
+			}
+		}
+	}
+
+	public function notify(int $timestamp) : void{
+		if($this->current !== null && $timestamp === $this->current->timestamp){
+			$this->processCurrent(true);
+		}
+	}
+}
---
+++ src/alvin0319/AmongUs/libs/muqsit/invmenu/session/PlayerSession.php
@@ -0,0 +1,145 @@
+<?php
+
+/*
+ *  ___            __  __
+ * |_ _|_ ____   _|  \/  | ___ _ __  _   _
+ *  | || '_ \ \ / / |\/| |/ _ \ '_ \| | | |
+ *  | || | | \ V /| |  | |  __/ | | | |_| |
+ * |___|_| |_|\_/ |_|  |_|\___|_| |_|\__,_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author Muqsit
+ * @link http://github.com/Muqsit
+ *
+*/
+
+declare(strict_types=1);
+
+namespace alvin0319\AmongUs\libs\muqsit\invmenu\session;
+
+use Closure;
+use InvalidArgumentException;
+use InvalidStateException;
+use alvin0319\AmongUs\libs\muqsit\invmenu\InvMenu;
+use alvin0319\AmongUs\libs\muqsit\invmenu\InvMenuHandler;
+use pocketmine\network\mcpe\protocol\types\ContainerIds;
+use pocketmine\Player;
+
+class PlayerSession{
+
+	/** @var Player */
+	protected $player;
+
+	/** @var PlayerNetwork */
+	protected $network;
+
+	/** @var MenuExtradata */
+	protected $menu_extradata;
+
+	/** @var InvMenu|null */
+	protected $current_menu;
+
+	/** @var int */
+	protected $current_window_id = ContainerIds::NONE;
+
+	public function __construct(Player $player, PlayerNetwork $network){
+		$this->player = $player;
+		$this->network = $network;
+		$this->menu_extradata = new MenuExtradata();
+	}
+
+	/**
+	 * @internal
+	 */
+	public function finalize() : void{
+		if($this->current_menu !== null){
+			$this->removeWindow();
+		}
+		$this->network->dropPending();
+	}
+
+	public function removeWindow() : void{
+		$window = $this->player->getWindow($this->current_window_id);
+		if($window !== null){
+			$this->player->removeWindow($window);
+			$this->network->wait(static function(bool $success) : void{});
+		}
+		$this->current_window_id = ContainerIds::NONE;
+	}
+
+	public function getMenuExtradata() : MenuExtradata{
+		return $this->menu_extradata;
+	}
+
+	private function sendWindow() : bool{
+		$this->removeWindow();
+
+		try{
+			$position = $this->menu_extradata->getPosition();
+			$inventory = $this->current_menu->getInventory();
+			/** @noinspection NullPointerExceptionInspection */
+			$inventory->moveTo($position->x, $position->y, $position->z);
+			$this->current_window_id = $this->player->addWindow($inventory);
+		}catch(InvalidStateException | InvalidArgumentException $e){
+			InvMenuHandler::getRegistrant()->getLogger()->debug("InvMenu failed to send inventory to {$this->player->getName()} due to: {$e->getMessage()}");
+			$this->removeWindow();
+		}
+
+		return $this->current_window_id !== ContainerIds::NONE;
+	}
+
+	/**
+	 * @internal use InvMenu::send() instead.
+	 *
+	 * @param InvMenu|null $menu
+	 * @param Closure|null $callback
+	 */
+	public function setCurrentMenu(?InvMenu $menu, ?Closure $callback = null) : void{
+		$this->current_menu = $menu;
+
+		if($this->current_menu !== null){
+			$this->network->waitUntil($this->network->getGraphicWaitDuration(), function(bool $success) use($callback) : void{
+				if($this->current_menu !== null){
+					if($success && $this->sendWindow()){
+						if($callback !== null){
+							$callback(true);
+						}
+						return;
+					}
+					$this->removeCurrentMenu();
+				}
+				if($callback !== null){
+					$callback(false);
+				}
+			});
+		}else{
+			$this->network->wait($callback ?? static function(bool $success) : void{});
+		}
+	}
+
+	public function getNetwork() : PlayerNetwork{
+		return $this->network;
+	}
+
+	public function getCurrentMenu() : ?InvMenu{
+		return $this->current_menu;
+	}
+
+	/**
+	 * @internal use Player::removeWindow() instead
+	 * @return bool
+	 */
+	public function removeCurrentMenu() : bool{
+		if($this->current_menu !== null){
+			$this->current_menu->getType()->removeGraphic($this->player, $this->menu_extradata);
+			$this->menu_extradata->reset();
+			$this->setCurrentMenu(null);
+			return true;
+		}
+		return false;
+	}
+}
