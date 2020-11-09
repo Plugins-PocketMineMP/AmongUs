581e457a16e7ad38ea9888ea43ce43be4eea159b
HydroGames-dev <69600115+HydroGames-dev@users.noreply.github.com>
2020-11-10 01:40:11 +0530

    Update 0.0.12

--- src/alvin0319/AmongUs/form/AmongUsMainForm.php
+++ src/alvin0319/AmongUs/form/AmongUsMainForm.php
@@ -76,7 +76,7 @@ class AmongUsMainForm implements Form{
 			    $player->sendMessage($lines . "\n" . "§8-=[§a+§8]§b=-§l§cAmong§eUs §r§ain §aMCPE §b-=§8[§a+§8]=-" . "\n" . $space . "\n" . "§6Intro:" . "\n" . $space . "\n" . "§eAmongUS is a game of teamwork & betrayal." . "\n" . "§ePlayers are either Crewmates or an Impostor." . "\n" . $space . "\n" . $space . "\n" . "§6Roles:" . "\n" . $space . "\n" . "§bCrewmate: Complete the tasks to win." . "\n" . "§cImposter: Kill all Crewmates to win." . "\n" . $space . "\n" . "§6Info:" . "\n" . $space . "\n" . "§eDuring Meetings make sure to discuss on who to vote out. (vote out the imposter)" . "\n" . $space . "§ePlayers have access to a personal map to help navigate through the map" . "\n" . $space . "§8-=[§a+§8]=- [§aHave Fun Playing§8] -=[§a+§8]=-" . "\n" . $lines);
 				break;
 			case 3:
-			    $line = "§8------------------------------------------------------";
+			    $lines = "§8------------------------------------------------------";
 			    $space = " ";
 			    $player->sendMessage($lines . "\n" . "§8-=[§a+§8]§b=-§l§cAmong§eUs §r§ain §aMCPE §b-=§8[§a+§8]=-" . "\n" . $space . "\n" . "§6Plugin Info:" . "\n" . $space . "\n" . "§6Name:" . "\n" . "§a§cAmong§eUs§r" . "\n" . $space . "\n" . "§6Author:" . "\n" . $space . "\n" . "§aAlvin0319" . "\n" . $space . "\n" . $space . "§8-=[§a+§8]=- [§cAmong§eUs §aby Alvin0319§8] -=[§a+§8]=-" . "\n" . $lines);
 				break;
