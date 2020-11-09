7df7d8e09f7bf351859e591e164b36df396dc8f5
HydroGames-dev <69600115+HydroGames-dev@users.noreply.github.com>
2020-11-10 01:11:42 +0530

    Add files via upload

---
+++ src/alvin0319/AmongUs/libs/muqsit/invmenu/session/network/NetworkStackLatencyEntry.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ *  ___            __  __
+ * |_ _|_ ____   _|  \/  | ___ _ __  _   _
+ *  | || '_ \ \ / / |\/| |/ _ \ '_ \| | | |
+ *  | || | | \ V /| |  | |  __/ | | | |_| |
+ * |___|_| |_|\_/ |_|  |_|\___|_| |_|\__,_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author Muqsit
+ * @link http://github.com/Muqsit
+ *
+*/
+
+declare(strict_types=1);
+
+namespace alvin0319\AmongUs\libs\muqsit\invmenu\session\network;
+
+use Closure;
+
+final class NetworkStackLatencyEntry{
+
+	/** @var int */
+	public $timestamp;
+
+	/** @var int */
+	public $network_timestamp;
+
+	/** @var Closure */
+	public $then;
+
+	public function __construct(int $timestamp, Closure $then, ?int $network_timestamp = null){
+		$this->timestamp = $timestamp;
+		$this->then = $then;
+		$this->network_timestamp = $network_timestamp ?? $timestamp;
+	}
+}
