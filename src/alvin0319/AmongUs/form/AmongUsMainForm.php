b2c7ccbd0903f7447a1ce6b35e7d439acf7deb53
HydroGames-dev <hydrogamesnetwork@gmail.com>
2020-11-10 16:34:27 +0530

    Test commit

--- src/alvin0319/AmongUs/form/AmongUsMainForm.php
+++ src/alvin0319/AmongUs/form/AmongUsMainForm.php
@@ -80,10 +80,11 @@ class AmongUsMainForm implements Form{
 			    $player->sendMessage($lines . "\n" . "§8-=[§a+§8]§b=-§l§cAmong§eUs §r§ain §aMCPE §b-=§8[§a+§8]=-" . "\n" . $space . "\n" . "§6Intro:" . "\n" . $space . "\n" . "§eAmongUS is a game of teamwork & betrayal." . "\n" . "§ePlayers are either Crewmates or an Impostor." . "\n" . $space . "\n" . $space . "\n" . "§6Roles:" . "\n" . $space . "\n" . "§bCrewmate: Complete the tasks to win." . "\n" . "§cImposter: Kill all Crewmates to win." . "\n" . $space . "\n" . "§6Info:" . "\n" . $space . "\n" . "§eDuring Meetings make sure to discuss on who to vote out. (vote out the imposter)" . "\n" . $space . "§ePlayers have access to a personal map to help navigate through the map" . "\n" . $space . "§8-=[§a+§8]=- [§aEnjoy Playing§8] -=[§a+§8]=-" . "\n" . $lines);
 				break;
 			case 3:
+			  $server = AmongUs::getInstance()->getServer();
 			  $game = AmongUs::getInstance()->getGameByPlayer($player);
 			  $spawn = AmongUs::getInstance()->getServer()->getDefaultLevel()->getSafeSpawn();
 			  $game->removePlayer($player);
-			  $this->AmongUs::getInstance()->getServer()->getDefaultLevel()->loadChunk($spawn->getX(), $spawn->getZ());
+			  $server->getDefaultLevel()->loadChunk($spawn->getX(), $spawn->getZ());
 			  $player->teleport($spawn, 0, 0);
 			  $player->sendMessage(AmongUs::$prefix . "You left the match.");
 			  break;
