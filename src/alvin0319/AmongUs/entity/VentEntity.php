8700a1f92ac2e43db767ffad99a090fc8dbae2c3
alvin0319 <alvin7810@naver.com>
2020-11-27 09:23:24 +0900

    VentEntity: Do not save with chunk

--- src/alvin0319/AmongUs/entity/VentEntity.php
+++ src/alvin0319/AmongUs/entity/VentEntity.php
@@ -9,6 +9,11 @@ use pocketmine\event\entity\EntityDamageEvent;
 
 class VentEntity extends Human{
 
+	protected function initEntity() : void{
+		parent::initEntity();
+		$this->setCanSaveWithChunk(false); // DO NOT SAVE ME!!!!!!!
+	}
+
 	public function attack(EntityDamageEvent $source) : void{
 		$source->setCancelled();
 	}
